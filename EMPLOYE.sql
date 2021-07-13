6768e6e5dc1e3e9b538d473cf9e76cc6cd2a7467
navicsoa <vicmeettrabajo@gmail.com>
2021-07-13 16:22:29 -0500

    Mi primer commit

---
+++ EMPLOYE.sql
@@ -0,0 +1,10 @@
+SELECT
+    "A1"."ID"             "ID",
+    "A1"."NOMBRE"         "NOMBRE",
+    "A1"."APELLIDO"       "APELLIDO",
+    "A1"."DEPARTAMENTO"   "DEPARTAMENTO"
+FROM
+    "SYSTEM"."TBL_EMPLOYEE" "A1";
+INSERT INTO TBL_EMPLOYEE(id,nombre,apellido,departamento)values(CLIENTE_PK.nextval,'PEDRO','RAMOS','RH');
+
+create SEQUENCE CLIENTE_PK;
