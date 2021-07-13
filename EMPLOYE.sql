b119150cdc9535f57e5e3218b36e6228e766978a
navicsoa <vicmeettrabajo@gmail.com>
2021-07-13 17:03:31 -0500

    Se modifica el registro de insert ,  add un registro

--- EMPLOYE.sql
+++ EMPLOYE.sql
@@ -5,6 +5,6 @@ SELECT
     "A1"."DEPARTAMENTO"   "DEPARTAMENTO"
 FROM
     "SYSTEM"."TBL_EMPLOYEE" "A1";
-INSERT INTO TBL_EMPLOYEE(id,nombre,apellido,departamento)values(CLIENTE_PK.nextval,'PEDRO','RAMOS','RH');
+INSERT INTO TBL_EMPLOYEE(id,nombre,apellido,departamento)values(CLIENTE_PK.nextval,'JUAN CARLOS','RAMOS','RH');
 
 create SEQUENCE CLIENTE_PK;
